9c437b6336dc153b237b0cde319234d91fd3c7e8
Paul Sijpkes <paul.sijpkes@gmail.com>
2018-03-20 17:18:00 +1100

    serving imgs - test

--- image.php
+++ image.php
@@ -10,7 +10,13 @@ $fn = array_pop($path);
 $f = (count($path) === 0) ? $fn : $f;
 
 header('Access-Control-Allow-Origin: *');
-header("Content-Type: image/png");
+//header("Content-Type: image/png");
 
-echo file_get_contents('files/'.$f);
+//echo file_get_contents('files/'.$f);
+
+$image = imagecreatefrompng($file);
+
+header('Content-Type: ' . image_type_to_mime_type(IMAGETYPE_WBMP));
+image2wbmp($image); // output the stream directly
+imagedestroy($image);
 ?>
