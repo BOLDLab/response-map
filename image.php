4aa07cd46c4d3f286ae76193143d9f87d09c2a33
Paul Sijpkes <paul.sijpkes@gmail.com>
2018-03-20 16:49:00 +1100

    serving imgs - test

--- image.php
+++ image.php
@@ -3,10 +3,16 @@ $url = parse_url($_SERVER['HTTP_HOST']);
 
 $f = $_GET['f'];
 
-$filename = explode('/',$f);
+$path = explode('/',$f);
 
-$filename = array_pop($filename);
+$fn = array_pop($path);
 
-echo $filename;
-//header("content-type: image/png");
+$f = (count($path === 0)) ? $fn : $f;
+
+echo $f;
+return;
+header("content-type: image/png");
+
+echo file_get_contents('files/'.$f);
+
 ?>
