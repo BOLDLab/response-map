8e4691488c4360148bed27d14e309dbd8c153fd4
Paul Sijpkes <paul.sijpkes@gmail.com>
2018-03-23 11:14:10 +1100

    back to JawsDB

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 	// Rename this file to config.php and enter in your MySQL database credentials
 
 	// MySQL database credentials
-	$host = '134.148.29.160';
-	$database_name = 'edx-heroku-hello-map';
-	$database_user = 'edx-heroku';
-	$database_pass = 'AXvsy5E7TcMy11XR';
+		$host = 'zwgaqwfn759tj79r.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+	$database_name = 'muvuw2rqcpm532tg';
+	$database_user = 'lx0agcf2cs4eejzf';
+	$database_pass = 'i89j1lrs12i76b87';
 	$adminpassword = 'ctlBOLD#nhi';
 
 	// Establish a connection to the database
