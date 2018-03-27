2007a2e523ae8b99cd0cb81de27f7db59d62d2c2
Paul Sijpkes <paul.sijpkes@gmail.com>
2018-03-27 16:59:15 +1100

    getenv

--- config.php
+++ config.php
@@ -9,11 +9,11 @@
 	$conn = mysqli_connect($host, $database_user, $database_pass, $database_name);
 
 	// Google Maps API key
-	$google_key = get_env('GOOGLE_API_KEY');
+	$google_key = getenv('GOOGLE_API_KEY');
 
-	$aws_access_key = get_env('AWS_ACCESS_KEY');
-	$aws_secret_key = get_env('AWS_SECRET_KEY');
-	$aws_s3_bucket = get_env('S3_BUCKET');
+	$aws_access_key = getenv('AWS_ACCESS_KEY');
+	$aws_secret_key = getenv('AWS_SECRET_KEY');
+	$aws_s3_bucket = getenv('S3_BUCKET');
 
 	$allowed_origins = ["https://edx.org", "https://studio.edx.org"];
 ?>
