c1397beea794c85c4d528739f9b3779897a5b9d0
Paul Sijpkes <paul.sijpkes@gmail.com>
2018-03-27 15:21:41 +1100

    added AWS S3 bucket

--- config.php
+++ config.php
@@ -2,17 +2,21 @@
 	// Rename this file to config.php and enter in your MySQL database credentials
 
 	// MySQL database credentials
-		$host = 'iwqrvsv8e5fz4uni.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-	$database_name = 'ee4r54etwrtw7ilw';
-	$database_user = 'dllphy9r18z7gdgl';
-	$database_pass = 'qf2r9wfczyu2ezwy';
-	$adminpassword = 'ctlBOLD#nhi';
+	$host = getenv('MYSQL_DB_HOST')
+	$database_name = getenv('MYSQL_DB_NAME');
+	$database_user = getenv('MYSQL_DB_USER');
+	$database_pass = getenv('MYSQL_DB_PASSWORD');
+	$adminpassword = getenv('DB_ADMIN_PASSWORD');
 
 	// Establish a connection to the database
 	$conn = mysqli_connect($host, $database_user, $database_pass, $database_name);
 
 	// Google Maps API key
-	$google_key = 'AIzaSyCyyijfRSuYHQDxtZREVk20YImRrwwu_Is';
+	$google_key = get_env('GOOGLE_API_KEY');
+
+	$aws_access_key = get_env('AWS_ACCESS_KEY');
+	$aws_secret_key = get_env('AWS_SECRET_KEY');
+	$aws_s3_bucket = get_env('S3_BUCKET');
 
 	$allowed_origins = ["https://edx.org", "https://studio.edx.org"];
 ?>
